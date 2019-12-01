94f6fe94626bfaaea2bfe5b39cc5a840f1ea9bbb
gowthami1702030 <58380329+gowthami1702030@users.noreply.github.com>
2019-12-01 13:21:40 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+app.set('view engine', 'ejs');
+console.log("hello");
+
+console.log("Welcome to first node program");
+app.get('/', function(req, res){
+         //res.send("Hello this is the first Node program");
+    res.render('index');
+});
+
+const PORT=process.env.PORT;
+const IP=process.env.IP;
+
+app.listen(PORT, IP, function(){
+console.log("Node server is up and running");
+});
